2e020fc391f947844974fd1275f8859161667337
Kristianto <inikristiantoitu@gmail.com>
2025-07-17 22:05:43 +0700

    (hide status bar in movie/[id])

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
 
 const MovieDetails = () => {
-  return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
-  )
-}
+	return (
+		<>
+			<StatusBar
+				hidden={true}
+				animated={true}
+				showHideTransition={'slide'}
+			/>
 
-export default MovieDetails
+			<View>
+				<Text>MovieDetails</Text>
+			</View>
+		</>
+	);
+};
+
+export default MovieDetails;
